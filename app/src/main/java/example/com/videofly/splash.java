af854dc25a887fdbb1d12b566419f3080bc61c91
Madhav Chhura <chhura.madhav@gmail.com>
2015-04-14 18:48:27 -0700

    Finished Splash Screen

--- app/src/main/java/example/com/videofly/splash.java
+++ app/src/main/java/example/com/videofly/splash.java
@@ -1,18 +1,89 @@
 package example.com.videofly;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
-public class splash extends ActionBarActivity {
+import android.os.Handler;
+import android.widget.RelativeLayout;
+
+
+public class splash extends Activity {
+
+    public void onAttachedToWindow(){
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                StartSplashScreenAnim();
+
+
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
+
+
+    private void StartSplashScreenAnim() {
+
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.splashLinLay);
+        layout.clearAnimation();
+        layout.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+        imageView.clearAnimation();
+        imageView.startAnimation(anim);
+
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent i = new Intent(splash.this, login.class);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
