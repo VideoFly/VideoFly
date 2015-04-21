5d263b9248a8f538ab7498b912d4fe327b1dcf35
Madhav Chhura <chhura.madhav@gmail.com>
2015-04-20 20:42:23 -0700

    Made changes to the facebook settings.

--- app/src/main/java/example/com/videofly/MainActivity.java
+++ app/src/main/java/example/com/videofly/MainActivity.java
@@ -1,10 +1,15 @@
 package example.com.videofly;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 
 
@@ -21,6 +26,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -32,11 +39,28 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        if(id == R.id.logout && ParseFacebookUtils.isLinked(ParseUser.getCurrentUser())){
+
+
+            ParseUser.logOut();
+
+
+            Toast.makeText(getApplicationContext(),"Log Out Successful",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this, login.class);
+            startActivity(i);
+            finish();
+        }
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/example/com/videofly/login.java
+++ app/src/main/java/example/com/videofly/login.java
@@ -2,12 +2,15 @@ package example.com.videofly;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.app.ProgressDialog;
+//import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -22,6 +25,7 @@ public class login extends Activity {
 
     private Dialog mProgressDialog;
 
+    //public static boolean loggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +35,78 @@ public class login extends Activity {
         setContentView(R.layout.activity_login);
 
 
+
         ImageButton mLoginButton = (ImageButton) findViewById(R.id.fb_login_button);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 userLogin();
             }
-
-
         });
 
 
     }
 
     private void userLogin() {
-        mProgressDialog = ProgressDialog.show(this, "Logging in", "Facebook", true);
+        //mProgressDialog = ProgressDialog.show(this, "Logging in", "Facebook", true);
 
-        List<String> permissions = Arrays.asList("basic_info");
+        List<String> permissions = Arrays.asList("user_friends");
 
         ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException err) {
+
                 if (user == null) {
-                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
-                } else if (user.isNew()) {
-                    Log.d("MyApp", "User signed up and logged in through Facebook!");
-                } else {
-                    Log.d("MyApp", "User logged in through Facebook!");
+                    Log.d("login-activity", "Uh oh. The user cancelled the Facebook login.");
+                    Toast.makeText(getApplicationContext(),"Unable to Login! Try Again", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (user.isNew()) {
+                    Log.d("login-activity", "User signed up and logged in through Facebook!");
+                    Toast.makeText(getApplicationContext(),"Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(login.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+
+                else {
+                    Log.d("login-activity", "User logged in through Facebook!");
+                    Toast.makeText(getApplicationContext(),"Login Successful! Try Again", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(login.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
                 }
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.login:
+                userLogin();
+            break;
+
+            case R.id.action_settings:
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
